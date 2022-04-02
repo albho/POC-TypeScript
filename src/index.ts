c134bce8f21eb270977c53815a9ef87dfaeeace4
Albert Ho <albertcyi.ho@gmail.com>
2022-04-02 09:52:03 -0700

    code - basic types, tuples, unions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
 
-// JavaScript:
-// let id = 5;
-let id: number = 5;
-// id = '5';
+// Basic Types
+let id: number = 28;
+let firstName: string = 'Albert';
+let likesCoffee: boolean = true;
+let x: any = 'Hi';
+
+let ids: number[] = [1, 2, 3, 4];
+let arr: any[] = [1, true, 'three'];
+
+// Tuple
+let person: [number, string, boolean] = [1, 'Albert', false];
+// types must be in same ordered
+let employee: [number, string][];
+
+employee = [
+  [1, 'Albert'],
+  [2, 'Bill'],
+]
+
+// Union
+let pid: string | number;
+pid = '22';
