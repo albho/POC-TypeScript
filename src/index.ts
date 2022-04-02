3d41cbde8f43a734c9cb5cf12d5de9763ddd0e1b
Albert Ho <albertcyi.ho@gmail.com>
2022-04-02 10:13:01 -0700

    code - interfaces

--- src/index.ts
+++ src/index.ts
@@ -60,3 +60,26 @@ console.log(answer)
 const log = (message: string | number): void => {
   console.log(message);
 }
+
+// Interfaces
+interface UserInterface {
+  readonly id: number,
+  name: string,
+  age?: number // ? for optional
+}
+
+const user1: UserInterface = {
+  id: 1,
+  name: 'Albert',
+}
+
+// * interface vs type *
+// use interface for objects, type for primitives & unions
+
+interface MathFunc {
+  (x: number, y: number): number;
+}
+
+const add: MathFunc = (x: number, y:number): number => x + y;
+const sub: MathFunc = (x: number, y:number): number => x - y;
+// const cannotDoThis: MathFunc = (x: number, y:string): number => x + y;
